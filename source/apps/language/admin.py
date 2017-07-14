b9b82acdf3b883b635c8c6670857760f3402e6ef
religofsil <religionofsilence@gmail.com>
2017-07-14 13:30:25 +0300

    come on

--- source/apps/language/admin.py
+++ source/apps/language/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from apps.custom_admin.admin import ImprovedRawIdFields
 from .models import Language
 
-admin.site.register(Language)
 
-hw='hello world'
+class LanguageAdmin(ImprovedRawIdFields):
+    list_display = ['name', 'initial', 'status']
+    search_fields = ['name']
 
+
+admin.site.register(Language, LanguageAdmin)
