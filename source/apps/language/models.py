d90b398e4511eb5c9c75c88f339c767b3abb908f
melanoya <alex250396@gmail.com>
2017-07-17 15:26:02 +0300

    language model update

--- source/apps/language/models.py
+++ source/apps/language/models.py
@@ -5,34 +5,38 @@ from lib.utils.managers import ActiveManager
 '''Here is simple model with choice'''
 
 
-# class Language(models.Model):
-#     ACTIVE = 'act'
-#     DEACTIVATED = 'deact'
-#     LANGUAGE_STATUS = (
-#         (ACTIVE, 'Active'),
-#         (DEACTIVATED, 'Deactivated'),
-#     )
-#     name = models.CharField(_('name'), max_length=15)
-#     initial = models.CharField(max_length=3)
-#     status = models.BooleanField(
-#         max_length=5,
-#         choices=LANGUAGE_STATUS,
-#         default=DEACTIVATED
-#     )
+class Language(models.Model):
+    ACTIVE = 'act'
+    DEACTIVATED = 'deact'
+    LANGUAGE_STATUS = (
+        (ACTIVE, 'Active'),
+        (DEACTIVATED, 'Deactivated'),
+    )
+    name = models.CharField(_('name'), max_length=15, unique=True)
+    initial = models.CharField(max_length=3)
+    status = models.CharField(
+        max_length=5,
+        choices=LANGUAGE_STATUS,
+        default=DEACTIVATED
+    )
+
+    def __unicode__(self):
+        return unicode(self.name)
 
 '''Here is model like model in users names'''
 
 
-class Language(models.Model):
-    name = models.CharField(_('name'), max_length=15, unique=True)
-    initial = models.CharField(max_length=3)
-    status = models.BooleanField(_('active'), default=False)
+# class Language(models.Model):
+#     name = models.CharField(_('name'), max_length=15, unique=True)
+#     initial = models.CharField(max_length=3)
+#     status = models.BooleanField(_('active'), default=False)
+#
+#     objects = ActiveManager()
+#
+#     def change(self, **kwargs):
+#         self.status = True
+#         self.save()
+#
+#     def __unicode__(self):
+#         return unicode(self.name)
 
-    objects = ActiveManager()
-
-    def change(self, **kwargs):
-        self.status = True
-        self.save()
-
-    # def __unicode__(self):
-    #     return unicode(self.status)
